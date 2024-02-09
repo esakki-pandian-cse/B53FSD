06074f3db3bc847f7170b2584b0c882021d59d9f
esakki-pandian-cse <aadhiranesakki96@gmail.com>
2024-02-09 11:03:14 +0530

    Updated

--- MEMORY GAME/script.js
+++ MEMORY GAME/script.js
@@ -1,4 +1,4 @@
-// getting and setting the elements and colors needed
+//! Getting and setting the elements and colors needed
 const boxes = document.querySelector(".boxes");
 const colors = [
   "aqua",
@@ -10,47 +10,77 @@ const colors = [
   "maroon",
   "hotpink",
 ];
-const colorList = [...colors,...colors]
-// console.log(colorList);
-const boxLength = colorList.length;
-// console.log(boxLength);
+const colorList = [...colors, ...colors];
+//console.log(colorList);
+const boxLength = colorList.length; //16
+//console.log(boxLength);
 
-// Initializing the main elements of the game state 
+//!Initializing the main elements of the game state
 let revealCount = 0;
-// ethana thadava flip pannirukom apdingura count
 let activeBox = null;
-// entha boxah currentah click pandrengurathuthan 
 let waitingTime = false;
-// box automaticah thirumpurathu than waiting time
 
-// function to display the boxes in the webpage
-function buildBoxes(color){
+//!function to display the boxes in the webpage
+function buildBoxes(color) {
   const element = document.createElement("div");
   element.classList.add("box");
-  element.setAttribute("data-color", color);
+  element.setAttribute("data-color", color); //!used for mapping the color
+  element.setAttribute("data-revealed", "false"); //!used to check revealed boxes
 
-// adding event listeners for click event
-element.addEventListener("click",()=>{
-  if(waitingTime){
-    return
-  }
-  element.style.backgroundColor = color
-})
+  //!adding event listeners for click event
+  element.addEventListener("click", () => {
+    const revealed = element.getAttribute("data-revealed");
 
+    //!display the boxes and checking for reveal and checking the current element
+    if (waitingTime || revealed === "true" || element == activeBox) {
+      return;
+    }
+    element.style.backgroundColor = color;
+
+    //!Checking the active box
+    if (!activeBox) {
+      activeBox = element;
+      return;
+    }
+    // console.log(activeBox);
+
+    //!Logic for matching color and winning condition
+    const colorMatch = activeBox.getAttribute("data-color");
+    if (colorMatch === color) {
+      activeBox.setAttribute("data-revealed", "true");
+      element.setAttribute("data-revealed", "true");
+      waitingTime = false;
+      activeBox = null;
+      revealCount += 2;
+      if (revealCount === boxLength) {
+        alert("Congratulations!😍 You Won!🎉🎉 Refresh to play again");
+      }
+      return;
+    }
+
+    //!change the waiting time to true and using settimeout for transistion
+    waitingTime = true;
+    setTimeout(() => {
+      element.style.backgroundColor = null;
+      activeBox.style.backgroundColor = null;
+      waitingTime = false;
+      activeBox = null;
+    }, 1000);
+  });
 
   return element;
-} 
+}
 
-// building the boxes for the game 
-for(let i = 0; i<boxLength; i++){
-  // this is the place where the colors are randomly displayed 
-  const randomIndex =  Math.floor(Math.random() * colorList.length);
+//!Building the boxes for the game
+for (let i = 0; i < boxLength; i++) {
+  //!this is the place where the colors are randomly displayed
+  const randomIndex = Math.floor(Math.random() * colorList.length);
   const color = colorList[randomIndex];
   const box = buildBoxes(color);
 
-// using splice method to avoid 3 repeated calls 
-  colorList.splice(randomIndex,1);
+//!using splice method to avoid 3 repeated calls
+  colorList.splice(randomIndex, 1);
 
-//  console.log(color);
-boxes.append(box);
+  //console.log(color);
+  boxes.append(box);
 }
